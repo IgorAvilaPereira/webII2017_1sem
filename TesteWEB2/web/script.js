1227afb96ef14ccc46d352ec183cc5ee26a7fb57
IgorAvilaPereira <igor.pereira@riogrande.ifrs.edu.br>
2017-03-01 21:08:08 -0300

    projeto teste

---
+++ TesteWEB2/web/script.js
@@ -0,0 +1,19 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+function helloworld() {
+    var n = document.getElementById("nome").value;
+    alert(n);
+    alert("HelloWorld!!!"+n);
+    
+}   
+
+function mostraCampoNome(){
+    var n = document.getElementById("nome").value;
+    alert(n);
+}
